0e5c1de49cb6458e8b9a3303e210d8351da77b34
Samuel <samuel.guichardon@icloud.com>
2024-06-13 19:50:06 +0200

    feat: create routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
+    use App\Http\Controllers\AuthController;
     use App\Http\Controllers\ProfileController;
-    use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Route;
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
 
-    Route::apiResource('/profile', ProfileController::class);
+    Route::post('/register',[AuthController::class,'register']);
+    Route::post('/login',[AuthController::class,'login'])->name('login');
+
+//    Route::apiResource('/profiles', ProfileController::class);
+    Route::get('/profile/{id}', [ProfileController::class,'show']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/create',[ProfileController::class,'create'])->name('create');
+        Route::put('/update/{id}',[ProFileController::class,'update'])->name('update');
+        Route::delete('/delete/{id}',[ProfileController::class,'delete'])->name('delete');
+        Route::post('/logout',[AuthController::class,'logout']);
+    });
+
