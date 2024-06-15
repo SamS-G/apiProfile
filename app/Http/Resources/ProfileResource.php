b17a92f6489a358a5fc1d9b39b928757c08ee387
Samuel <samuel.guichardon@icloud.com>
2024-06-15 13:45:09 +0200

    feat: add guard on status column for none admin user

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileResource extends JsonResource
 {
@@ -19,9 +20,9 @@ class ProfileResource extends JsonResource
             'last_name' => $this->last_name,
             'first_name' => $this->first_name,
             'avatar' => $this->avatar,
+            ...Auth::guard('sanctum')->check() ? ['status' => $this->status] : [], // If the current user is authenticated, status of profiles are visibles.
             'created_at' => $this->created_at->format('d-m-Y '),
-            'updated_at' => $this->updated_at->format('d-m-Y '),
-
+//            'updated_at' => $this->updated_at->format('d-m-Y '),
         ];
     }
 }
