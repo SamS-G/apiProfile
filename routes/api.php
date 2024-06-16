3bc5dcd716fed4c70e1f948d501ed944a3abc7d6
Samuel <samuel.guichardon@icloud.com>
2024-06-16 16:50:14 +0200

    chore: storing routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,19 @@
 <?php
 
-    use App\Http\Controllers\AuthController;
-    use App\Http\Controllers\CreateProfileController;
-    use App\Http\Controllers\EditProfileController;
-    use App\Http\Controllers\ProfileController;
-    use App\Http\Controllers\DeleteProfileController;
-    use App\Http\Controllers\ShowProfileController;
+    use App\Http\Controllers\Authenticate\AuthController;
+    use App\Http\Controllers\Profile\CreateProfileController;
+    use App\Http\Controllers\Profile\DeleteProfileController;
+    use App\Http\Controllers\Profile\EditProfileController;
+    use App\Http\Controllers\Profile\IndexProfileController;
+    use App\Http\Controllers\Profile\ShowProfileController;
     use Illuminate\Support\Facades\Route;
+
+// Public endpoints
+
+    // This endpoint show all actives profiles without "status" column if current user are not logged in
+    Route::get('/profiles', [IndexProfileController::class, 'index'])->name('profiles');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 // Secured endpoints, need active account and logged in
 
@@ -17,16 +24,10 @@
                 Route::post('/create', [CreateProfileController::class, 'create'])->name('create');
                 Route::put('/edit', [EditProfileController::class, 'edit'])->name('edit');
                 Route::delete('/delete/{id}', [DeleteProfileController::class, 'delete'])->name('delete');
+                Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
             }
         });
     });
 
-// Public endpoints
-
-    // This endpoint show all actives profiles without "status" column if current user are not logged in
-    Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/logout', [AuthController::class, 'logout']);
 
 
