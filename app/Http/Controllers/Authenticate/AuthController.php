d23a07fe15fea8c05a21bedc7088d490cbc096bb
Samuel <samuel.guichardon@icloud.com>
2024-08-08 11:45:21 +0200

    refacto: authcontroller with new response class

--- app/Http/Controllers/Authenticate/AuthController.php
+++ app/Http/Controllers/Authenticate/AuthController.php
@@ -2,22 +2,23 @@
 
     namespace App\Http\Controllers\Authenticate;
 
-    use App\Http\Controllers\BaseController;
+    use App\Http\Exceptions\RequestException;
     use App\Http\Requests\Authenticate\LoginRequest;
     use App\Http\Requests\Authenticate\RegisterRequest;
+    use App\Http\Responses\API\ApiErrorResponse;
+    use App\Http\Responses\API\ApiSuccessResponse;
     use App\Models\User;
     use App\Repository\User\UserRepository;
     use Exception;
-    use Illuminate\Http\JsonResponse;
     use Illuminate\Support\Facades\Hash;
 
-    class AuthController extends BaseController
+    class AuthController
     {
         /*
         *   Register a new User.
         *   Before manual validation is not an administrator and it is not active
         */
-        public function register(RegisterRequest $request): JsonResponse
+        public function register(RegisterRequest $request): ApiErrorResponse|ApiSuccessResponse
         {
             try {
                 $user = [
@@ -29,12 +30,12 @@
 
                 (new UserRepository())->create($user);
 
-                return $this->apiResponse->success("New user successfully created, need activated by admin for full features access", 201, [
+                return new ApiSuccessResponse([
                     'user' => $user
-                ]);
-
+                ],
+                    "New user successfully created, need activated by admin for full features access", 201);
             } catch (Exception $e) {
-                return $this->apiResponse->error($e->getMessage(), 500);
+                return new ApiErrorResponse($e->getMessage(), $e);
             }
         }
 
@@ -42,7 +43,10 @@
          * Logs in as administrator and sends a new token to access protected endpoints.
          * The token is valid for 1 hour.
          */
-        public function login(LoginRequest $request): JsonResponse
+        /**
+         * @throws RequestException
+         */
+        public function login(LoginRequest $request): ApiSuccessResponse|ApiErrorResponse
         {
             $identifiers = [
                 'email' => $request->validated('email'),
@@ -52,30 +56,31 @@
             $user = User::where('email', $identifiers['email'])->first();
 
             if (is_null($user)) {
-                return $this->apiResponse->error("User not found", 404, [
+                return new ApiSuccessResponse([
                     "userMail" => $identifiers['email']
-                ]);
+                ], "User not found", 404);
             }
 
             if (Hash::check($identifiers['password'], $user->password) && ($user->is_active)) {
                 $expirationTime = now()->addMinutes(60);
                 $token = $user->createToken(time(), ['*'], $expirationTime)->plainTextToken;
 
-                return $this->apiResponse->success("Logged in successfully, use your Bearer token to authenticate on protected endpoints", 200, [
+                return new ApiSuccessResponse([
                     "token" => $token,
                     'expiration_date' => $expirationTime->format('d-m-Y H:i:s')
-                ]);
+                ],
+                    "Logged in successfully, use your Bearer token to authenticate on protected endpoints", 200);
             }
-            return $this->apiResponse->error('Login failed', 422);
+            return new ApiErrorResponse('Login failed', throw new RequestException());
         }
 
         /*
          * Revoke token, protected end points are closed for current user
          */
-        public function logout(): JsonResponse
+        public function logout(): ApiSuccessResponse
         {
             auth()->user()->tokens()->delete();
 
-            return $this->apiResponse->success("Logged out, token revoked", 200, []);
+            return new ApiSuccessResponse([], "Logged out, token revoked", 200);
         }
     }
