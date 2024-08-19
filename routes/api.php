a62cc47a4ddb82c334e72cec2bc47c198f84c0cd
Samuel <samuel.guichardon@icloud.com>
2024-08-19 16:17:01 +0200

    refacto: add register new user endpoint to protected

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@
     // This endpoint show all actives profiles without "status" column if current user are not logged in
     Route::get('/profiles', [IndexProfileController::class, 'index'])->name('profiles');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
@@ -24,6 +23,7 @@
     Route::prefix('profile')->group(function () {
         Route::middleware('auth:sanctum')->group(function () {
             {
+                Route::post('/register', [AuthController::class, 'register'])->name('register');
                 Route::get('/name', [ShowProfileController::class, 'showByName'])->name('name');
                 Route::post('/create', [CreateProfileController::class, 'create'])->name('create');
                 Route::put('/edit', [EditProfileController::class, 'edit'])->name('edit');
